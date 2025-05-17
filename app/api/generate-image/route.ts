2229b895bc6ef0f19d7bf1c733f9b41bc77cedec
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-05-17 09:47:25 +0900

    依存関係を更新し、axiosとform-dataを追加しました。また、react-day-pickerのバージョンを9.7.0に更新し、@auth/coreのバージョンを0.39.1に変更しました。ImageGeneratorコンポーネントに画像生成機能を追加し、フォームのactionをgenerateImage関数に設定しました。

---
+++ app/api/generate-image/route.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import FormData from 'form-data';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const { keyword } = await request.json();
+
+  try {
+    const payload = {
+      prompt: `Create Image with ${keyword}`,
+      output_format: 'png',
+    };
+
+    const formData = new FormData();
+    formData.append('prompt', payload.prompt);
+    formData.append('output_format', payload.output_format);
+
+    const response = await axios.post(
+      `https://api.stability.ai/v2beta/stable-image/generate/core`,
+      formData,
+      {
+        validateStatus: undefined,
+        responseType: 'arraybuffer',
+        headers: {
+          Authorization: `Bearer ${process.env.STABILITY_API_KEY}`,
+          Accept: 'image/*',
+        },
+      }
+    );
+
+    if (response.status !== 200) {
+      throw new Error(`API error: ${process.env.STABILITY_API_KEY}`);
+    }
+
+    console.info(response.data);
+  } catch (error) {
+    console.error(`Error generate image:${error}`);
+
+    return NextResponse.json(
+      {
+        error: 'Failed to generate image',
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- components/dashboard/tools/image-generator.tsx
+++ components/dashboard/tools/image-generator.tsx
@@ -3,10 +3,24 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 const ImageGenerator = () => {
+  async function generateImage(formData: FormData) {
+    'use server';
+    const keyword = formData.get('keyword');
+    const response = await fetch(`${process.env.BASE_URL}/api/generate-image`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ keyword }),
+    });
+
+    console.log(response);
+  }
+
   return (
     <div className="space-y-6">
       <div className="space-y-4">
-        <form action="" className="space-y-4">
+        <form action={generateImage} className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="prompt">キーワード</Label>
             <Input
